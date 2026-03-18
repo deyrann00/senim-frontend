693e1f6d5f20724e13a11ea9cf73941862cbccb0
d1rannn <denchik.stars06@gmail.com>
2026-03-18 17:19:00 +0500

    fix: file locations

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// If you have a global CSS file, uncomment the line below:
-// import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
